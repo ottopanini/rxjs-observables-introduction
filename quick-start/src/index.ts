e5c12896c53d3de0a3b8b8ea67e3f7af8dc90fcb
Christian Preß <cpress@it4ipm.de>
2022-04-23 18:34:22 +0200

    add subscription

--- quick-start/src/index.ts
+++ quick-start/src/index.ts
@@ -9,3 +9,9 @@ name$.subscribe(value => console.log(value));
 
 // storeDataOnServer returns an observable. nothing happens so far
 storeDataOnServer('Some value');
+
+// with subscription...
+storeDataOnServer('Some value').subscribe(value => console.log(value));
+// it stores the value
+
+
