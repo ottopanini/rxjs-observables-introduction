bd1017e7eca9d17504884d056187666ccd5c3f42
Christian Preß <cpress@it4ipm.de>
2022-04-23 18:32:09 +0200

    missing subscription

--- quick-start/src/index.ts
+++ quick-start/src/index.ts
@@ -4,4 +4,8 @@ import {
   storeDataOnServerError
 } from './external';
 
+// emits 3 values
+name$.subscribe(value => console.log(value)); 
 
+// storeDataOnServer returns an observable. nothing happens so far
+storeDataOnServer('Some value');
