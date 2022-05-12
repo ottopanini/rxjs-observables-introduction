c531f161ab4b0cc2bf38cf7f5e2c28ac984d402c
Christian Preß <cpress@it4ipm.de>
2022-05-13 00:56:55 +0200

    add observable

--- warm-up observable/src/index.ts
+++ warm-up observable/src/index.ts
@@ -1,4 +1,13 @@
+import {Observable} from 'rxjs';
 
+new Observable<string>(subscriber => {
+    console.log('Observable executed');
+    subscriber.next('Alice');
+    subscriber.next('Ben');
+    subscriber.next('Charlie');
+});
+
+// -> nothing happens
 
 
 
